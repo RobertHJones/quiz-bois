6fabfa9df4c0f5444b719e6c3005e35303ba5ca7
Scott Wilson <swrs@hotmail.co.uk>
2022-05-20 11:28:25 +0100

    managed to fetch data back from api

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
 import Quizbox from "../Quizbox";
+import { useEffect } from "react";
+
+const quizAPI = {
+  key: process.env.REACT_APP_API_KEY,
+  url: "https://quizapi.io/api/v1/questions1",
+  limit: 10,
+};
 
 function App() {
+  async function fetchQuiz() {
+    const response = await fetch(
+      // `${quizAPI.url}/${quizAPI.key}/${quizAPI.limit}`
+      `https://quizapi.io/api/v1/questions`,
+      {
+        headers: {
+          "X-Api-Key": "SvibEz6MaaoNDKqkpmjkLWJQylK0zEsWVX05Hnqm",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  }
+  useEffect(() => {
+    fetchQuiz();
+  }, []);
+
   return (
     <div className="App">
       <h1>The Big Quiz</h1>

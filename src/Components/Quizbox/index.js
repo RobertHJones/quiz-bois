99cc00947d75be0ce190b51a5ba0e4d1efe7464d
RobertHJones <roberthuwjones@outlook.com>
2022-05-20 14:09:29 +0100

    questions + answers loading

--- src/Components/Quizbox/index.js
+++ src/Components/Quizbox/index.js
@@ -4,14 +4,22 @@ import "./index.css";
 export default function Quizbox({ data }) {
   return (
     <section className="quiz">
-      <div className="question-container">
-        <h2>{data.results[0].question}</h2>
-      </div>
+      {data !== undefined && (
+        <div className="question-container">
+          <h2>{data.results[0].question}</h2>
+        </div>
+      )}
       <div className="answers-container">
-        {/* <button className="answer-a">{data[0].answers.answer_a}</button>
-        <button className="answer-b">{data[0].answers.answer_b}</button>
-        <button className="answer-c">{data[0].answers.answer_c}</button>
-        <button className="answer-d">{data[0].answers.answer_d}</button> */}
+        <button className="answer-a">{data.results[0].correct_answer}</button>
+        <button className="answer-b">
+          {data.results[0].incorrect_answers[0]}
+        </button>
+        <button className="answer-c">
+          {data.results[0].incorrect_answers[1]}
+        </button>
+        <button className="answer-d">
+          {data.results[0].incorrect_answers[2]}
+        </button>
       </div>
       <div className="scrolling">
         <button className="previous">Previous question</button>
